3f9143232c479c9b70141d73cb005d99671c9c0a
Rick Drizin <Drizin@users.noreply.github.com>
2020-11-30 12:03:35 -0300

    AdventureWorks.Core.CoreDatabase:   CoreDbReadConnection / CoreDbReadWriteConnection   CodeGenerator (CodegenCS scripts) to extract AdventureWorks db schema and generate Entities + CRUD methods (currently unused since we're using Dapper FastCRUD)   Entities are generated in Domain Model

---
+++ src/AdventureWorks.Core.CoreDatabase/CoreDbReadConnection.cs
@@ -0,0 +1,18 @@
+﻿using Harbin.Infrastructure.Database.Connection;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AdventureWorks.Core.CoreDatabase
+{
+    public partial class CoreDbReadConnection : ReadDbConnection
+    {
+        public CoreDbReadConnection(IDbConnection dbConnection) : base(dbConnection)
+        {
+        }
+        public CoreDbReadConnection(IDbConnectionFactory connFactory) : this(connFactory.CreateConnection())
+        {
+        }
+    }
+}
---
+++ src/AdventureWorks.Core.CoreDatabase/CoreDbReadWriteConnection.cs
@@ -0,0 +1,24 @@
+﻿using Harbin.Infrastructure.Database.Connection;
+using Harbin.Infrastructure.Database.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AdventureWorks.Core.CoreDatabase
+{
+    public partial class CoreDbReadWriteConnection : ReadWriteDbConnection
+    {
+        public CoreDbReadWriteConnection(IDbConnection dbConnection) : base(dbConnection)
+        {
+        }
+        public CoreDbReadWriteConnection(IDbConnectionFactory connFactory) : this(connFactory.CreateConnection())
+        {
+        }
+
+        /// <summary>
+        /// If there are reusable Query Objects or Query Methods in your ReadDbConnection, you can reuse those methods (with same connection)
+        /// </summary>
+        public virtual CoreDbReadConnection CoreDbReadConnection { get { return new CoreDbReadConnection(_dbConnection); } }
+    }
+}
